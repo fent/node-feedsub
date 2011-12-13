c322071a94a227245c4084c2968ca985998ed678
Roly Fentanes <roly426@gmail.com>
2011-12-13 12:22:31 -0800

    much better tests

--- test/test.js
+++ test/test.js
@@ -5,17 +5,29 @@ var FeedSub = require('./../lib/feedsub');
 exports['Read the old rss feed first'] = function(beforeExit, assert) {
   var host = 'http://feedsite.info'
     , path = '/rss/feed.xml'
-    , reader = new FeedSub(host + path, { interval: 3 })
-    , n = 0
+    , reader = new FeedSub(host + path, { emitOnStart: true })
+    , callbackCount = 0
+    , itemCount = 0
+    , itemsEvents = 0
     ;
 
   nock(host)
     .get(path)
     .replyWithFile(200, __dirname + '/feedold.xml')
 
+  reader.on('item', function(item) {
+    itemCount++;
+  });
+
+  reader.on('items', function(items) {
+    itemsEvents++;
+    var n = itemsEvents === 1 ? 2997 : 3;
+    assert.eql(items.length, n);
+  });
+
   reader.read(function(err, items) {
     if (err) throw err;
-    assert.eql(++n, 1);
+    assert.eql(++callbackCount, 1);
     assert.isNull(err);
     assert.eql(items.length, 2997, 'read all items in feed');
 
@@ -26,7 +38,7 @@ exports['Read the old rss feed first'] = function(beforeExit, assert) {
 
     reader.read(function(err, items) {
       if (err) throw err;
-      assert.eql(++n, 2);
+      assert.eql(++callbackCount, 2);
       assert.isNull(err);
       assert.eql(items.length, 0, 'should not return any new items');
 
@@ -37,11 +49,19 @@ exports['Read the old rss feed first'] = function(beforeExit, assert) {
 
       reader.read(function(err, items) {
         if (err) throw err;
-        assert.eql(++n, 3);
+        assert.eql(++callbackCount, 3);
         assert.isNull(err);
         assert.eql(items.length, 3, '3 new items');
       });
+
     });
+
+  });
+
+  beforeExit(function() {
+    assert.eql(callbackCount, 3);
+    assert.eql(itemCount, 3000);
+    assert.eql(itemsEvents, 2);
   });
 };
 
@@ -49,18 +69,34 @@ exports['Read feed without emitOnStart'] = function(beforeExit, assert) {
   var host = 'http://feedsite.info'
     , path = '/rss/feed.xml'
     , reader = new FeedSub(host + path, { emitOnStart: false })
-    , n = 0
+    , callbackCount = 0
+    , itemCount = 0
+    , itemsEvents = 0
     ;
 
   nock(host)
     .get(path)
     .replyWithFile(200, __dirname + '/feedold.xml')
 
+  reader.on('item', function() {
+    itemCount++;
+  });
+
+  reader.on('items', function() {
+    itemsEvents++;
+  });
+
   reader.read(function(err, items) {
     if (err) throw err;
-    assert.eql(++n, 1);
+    assert.eql(++callbackCount, 1);
     assert.isNull(err);
     assert.eql(items.length, 0, 'should return no items');
+  });
+
+  beforeExit(function() {
+    assert.eql(callbackCount, 1);
+    assert.eql(itemCount, 0);
+    assert.eql(itemsEvents, 0);
   });
 };
 
@@ -76,19 +112,37 @@ exports['Use skipHours'] = function(beforeExit, assert) {
 
   var host = 'http://www.google.com'
     , path = '/reader/public/atom/'
-    , reader = new FeedSub(host + path, { skipHours: true })
-    , n = 0
+    , reader = new FeedSub(host + path, {
+        emitOnStart: true, skipHours: true
+      })
+    , callbackCount = 0
+    , itemCount = 0
+    , itemsEvents = 0
     ;
 
   nock(host)
     .get(path)
     .replyWithFile(200, __dirname + '/aninews.rss')
 
+  reader.on('item', function() {
+    itemCount++;
+  });
+
+  reader.on('items', function() {
+    itemsEvents++;
+  });
+
   reader.read(function(err, items) {
     if (err) throw err;
-    assert.eql(++n, 1);
+    assert.eql(++callbackCount, 1);
     assert.isNull(err);
     assert.eql(items.length, 0, 'should return no items');
+  });
+
+  beforeExit(function() {
+    assert.eql(callbackCount, 1);
+    assert.eql(itemCount, 0);
+    assert.eql(itemsEvents, 0);
   });
 };
 
@@ -96,17 +150,27 @@ exports['Use skipHours'] = function(beforeExit, assert) {
 exports['Same title but different pubdate'] = function(beforeExit, assert) {
   var host = 'http://feedburner.info'
     , path = '/rss'
-    , reader = new FeedSub(host + path)
-    , n = 0
+    , reader = new FeedSub(host + path, { emitOnStart: true })
+    , callbackCount = 0
+    , itemCount = 0
+    , itemsEvents = 0
     ;
 
   nock(host)
     .get(path)
     .replyWithFile(200, __dirname + '/rss2old.xml')
 
+  reader.on('item', function() {
+    itemCount++;
+  });
+
+  reader.on('items', function() {
+    itemsEvents++;
+  });
+
   reader.read(function(err, items) {
     if (err) throw err;
-    assert.eql(++n, 1);
+    assert.eql(++callbackCount, 1);
     assert.isNull(err);
     assert.eql(items.length, 4, 'read all items in feed');
 
@@ -117,7 +181,7 @@ exports['Same title but different pubdate'] = function(beforeExit, assert) {
 
     reader.read(function(err, items) {
       if (err) throw err;
-      assert.eql(++n, 2);
+      assert.eql(++callbackCount, 2);
       assert.isNull(err);
       assert.eql(items.length, 0, 'should not return any new items');
 
@@ -128,10 +192,16 @@ exports['Same title but different pubdate'] = function(beforeExit, assert) {
 
       reader.read(function(err, items) {
         if (err) throw err;
-        assert.eql(++n, 3);
+        assert.eql(++callbackCount, 3);
         assert.isNull(err);
         assert.eql(items.length, 1, '1 new item with different pubdate');
       });
     });
+  });
+
+  beforeExit(function() {
+    assert.eql(callbackCount, 3);
+    assert.eql(itemCount, 5);
+    assert.eql(itemsEvents, 2);
   });
 };
